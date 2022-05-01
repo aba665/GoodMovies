a3d9fe44d39febedd70e7210b86545b01973df17
aba665 <abissons@gmail.com>
2022-04-30 22:06:43 -0300

    Inicio do projeto utilizando o react

---
+++ site_movies/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyTest from './Components/welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyTest />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ site_movies/src/Components/welcome.js
@@ -0,0 +1,12 @@
+
+function MyTest(){
+    return (
+        <div>
+            <h1>Olá seja bem vindo ao meu teste!</h1>
+            <button className="allButton">+</button><span>0</span><button className="allButton">-</button>
+        </div>
+    )
+
+}
+
+export default MyTest;
