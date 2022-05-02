3a2a36f0ddc38b40a9d560a99b7b8f4e8ce91a73
aba665 <abissons@gmail.com>
2022-05-02 20:16:56 -0300

    Criando o componente de estrutura da página de login

---
+++ site_movies/src/components/StructureLogin/structure.Test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Teste do compontente estrutura de login', () => {
+    test('Deve conter um formulário', () => {
+        
+    })
+
+})
